bd095cc5b7d7c3b57dc13454a432f7ba3b4d4105
blakejosh <214008066+blakejosh54@users.noreply.github.com>
2026-02-13 18:58:43 +0200

    responsiveness complete JS scroll behaviour added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Tweet input resize function
+// tweet input resize function
 document.addEventListener("DOMContentLoaded", () => {
   const textarea = document.querySelector(".composer-input");
 
@@ -12,4 +12,43 @@ document.addEventListener("DOMContentLoaded", () => {
   textarea.addEventListener("input", autoGrow);
 
   autoGrow();
+});
+
+
+// navbar, topbar and post button slide away function
+
+document.addEventListener("DOMContentLoaded", () => {
+  const topbar = document.querySelector(".topbar");
+  const bottomNav = document.querySelector(".mobile-nav");
+  const fab = document.querySelector(".fab-post");
+
+  let lastScrollY = window.scrollY;
+  let scrollAccumulator = 0;
+  const threshold = 40;
+
+  window.addEventListener("scroll", () => {
+    const currentScrollY = window.scrollY;
+    const scrollDifference = currentScrollY - lastScrollY;
+
+    scrollAccumulator += scrollDifference;
+
+    // scroll down
+    if (scrollAccumulator > threshold) {
+      topbar?.classList.add("hide");
+      bottomNav?.classList.add("hide");
+      fab?.classList.add("hide");
+
+      scrollAccumulator = 0;
+    }
+
+    // scroll up
+    if (scrollAccumulator < -threshold) {
+      topbar?.classList.remove("hide");
+      bottomNav?.classList.remove("hide");
+      fab?.classList.remove("hide");
+
+      scrollAccumulator = 0;
+    }
+    lastScrollY = currentScrollY;
+  });
 });
